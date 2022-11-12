c6ff450376c964acf344db76c37d6b2165bf0f2a
D4NZ-jpg <57308957+D4NZ-jpg@users.noreply.github.com>
2022-11-12 17:41:17 -0600

    Fix Chapter 1 Typo

--- TinyRenderer/src/main.cpp
+++ TinyRenderer/src/main.cpp
@@ -1,7 +1,12 @@
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+Model* model = NULL;
+const int width = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 {
@@ -34,6 +39,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 			image.set(x, y, color);
 		
 		error += derror;
+		if(error > dx)
 		{
 			y += (y1 > y0 ? 1 : -1);
 			error -= dx*2;
@@ -42,13 +48,25 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 }
 
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
+	TGAImage image(width, height, TGAImage::RGB);
+	model = new Model("./assets/african_head.obj");
 
-	line(13, 20, 80, 40, image, white);
-	line(20, 13, 40, 80, image, red);
-	line(80, 40, 13, 20, image, red);
+	for (int i = 0; i < model->nfaces(); i++)
+	{
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j < 3; j++) {
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1.) * width / 2.;
+			int y0 = (v0.y + 1.) * height / 2.;
+			int x1 = (v1.x + 1.) * width / 2.;
+			int y1 = (v1.y + 1.) * height / 2.;
+			line(x0, y0, x1, y1, image, white);
+		}
+	}
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
+	delete model;
 	return 0;
 }
